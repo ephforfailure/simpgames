392d438e029749874475f3686f464b20b117a4b5
ephforfailure <ephforfailure@gmail.com>
2025-10-22 11:42:41 -0700

    implemented win state

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -4,7 +4,7 @@
 #include <bool.h>
 #include <math.h>
 
-
+/* NOTE The code presume these values are the same. */
 #define BOARD_WIDTH  3
 #define BOARD_HEIGHT 3
 
@@ -17,6 +17,7 @@ typedef enum {
 typedef struct {
 	player_e current_player;
 	player_e* board;
+
 } game_t;
 
 int board_area(void) {
@@ -27,10 +28,10 @@ game_t* new_game(void) {
 	game_t* game = malloc(sizeof(game_t));
 	assert(game);
 	game.table = malloc(sizeof(player_e) * board_area());
-	assert(game.table);
+	assert(game->table);
 	int i = 0;
 	for (;i<board_area(); i++) {
-		game.table[i] = PLAYER_ZERO;
+		game->table[i] = PLAYER_ZERO;
 	}
 	game.current_player = PLAYER_X;
 	return game;
@@ -45,13 +46,13 @@ int board_get_index(int x, int y) {
 
 bool game_is_cell_zero(game_t* game, int x, int y) {
 	int index = board_get_index(x, y);
-	return game.board[index] == PLAYER_ZERO;
+	return game->board[index] == PLAYER_ZERO;
 }
 
 void game_move(game_t* game, int x, int y) {
 	if game_is_cell_zero(game, x, y) {
 		int index = board_get_index(x, y);
-		game.board[index] = game.current_player;
+		game->board[index] = game.current_player;
 	} else {
 		assert(false);
 	}
@@ -70,13 +71,13 @@ bool game_is_won(game_t* game) {
 	int y, x = 0, 0;
 
 	/* check for row wins */
-	for (y=0; y<BOARD_WIDTH, y++) { 
+	for (y=0; y<BOARD_WIDTH; y++) { 
 		is_won = true;
 		int index = board_get_index(0, y);
-		player_e player_row = game->board[index];
+		player_e player = game->board[index];
 		for (x=0; x<BOARD_HEIGHT, x++) {
 			index = board_get_index(x, y);
-			if (player_row != board[index]) {
+			if (player != board[index]) {
 				is_won = false;
 				break;
 			}
@@ -87,10 +88,10 @@ bool game_is_won(game_t* game) {
 	}
 
 	/* check for column wins */
-	for (x=0; x<BOARD_HEIGHT, x++) { 
+	for (x=0; x<BOARD_HEIGHT; x++) { 
 		is_won = true;
 		int index = board_get_index(x, 0);
-		player_e player_row = game->board[index];
+		player_e player = game->board[index];
 		for (y=0; y<BOARD_WIDTH, y++) {
 			index = board_get_index(x, y);
 			if (player_row != board[index]) {
@@ -103,8 +104,48 @@ bool game_is_won(game_t* game) {
 		}
 	}
 
+	if (BOARD_HEIGHT != BOARD_WIDTH) { // ignore cross wins if the board is not squared.
+		return false;
+	}
+
 	/* check for cross wins */
 
+	/*
+	 * -------------
+	 * | 0 | 1 | 2 |
+	 * | 3 | 4 | 5 |
+	 * | 6 | 7 | 8 |
+	 * -------------
+	 *  0, 4, 8
+	 *  2, 4, 6
+	 *  -----------
+	 *
+	 */
+	int index;
+	player_e player = game->board[0];
+	for (index=0; index<board_area(); index += (BOARD_WIDTH+1)) {
+		is_won = true;
+		if player != game->board[index] {
+			is_won = false;
+			continue;
+		}
+		if (is_won) {
+			return is_won;
+		}
+	}
+
+
+	for (index=(BOARD_WIDTH-1); index<board_area(); index -= (BOARD_WIDTH-1)) {
+		is_won = true;
+		if player != game->board[index] {
+			is_won = false;
+			continue;
+		}
+		if (is_won) {
+			return is_won;
+		}
+	}
+	return false;
 }
 
 
